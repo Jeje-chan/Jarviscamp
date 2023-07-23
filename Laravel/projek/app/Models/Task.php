66a4d426ccec5e6151b91556b51ea490b8ce39b0
Jeje-chan <sharmaazzahrah02@gmail.com>
2023-07-23 09:46:34 +0700

    "pertemuan 6"

---
+++ Laravel/projek/app/Models/Task.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    private static $tasks = [
+        [
+            'id' => 1,
+            'name' => 'Tugas 2 Statistika',
+            'description' => 'Membuat distribusi frekuensi',
+            'deadline' => '2023-03-22',
+            'status' => 'Selesai'
+        ],
+        [
+            'id' => 2,
+            'name' => 'Tugas 3 Statistika',
+            'description' => 'Membuat Korelasi Data',
+            'deadline' => '2023-04-20',
+            'status' => 'Selesai'
+        ],
+        [
+            'id' => 3,
+            'name' => 'Tugas 4 Statistika',
+            'description' => 'Membuat Regresi Data',
+            'deadline' => '2023-05-25',
+            'status' => 'Selesai'
+        ],
+    ];
+    public static function getAll(){
+        return self::$tasks;
+    }
+} 
